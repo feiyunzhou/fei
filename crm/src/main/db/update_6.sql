e5eb3e4a08124bbe5bd76e32b66594067c9c2e9a
Feiyun <feiyun.zhou@gmail.com>
2013-10-29 23:16:46 +0800

    fix error in sql

--- crm/src/main/db/update_6.sql
+++ crm/src/main/db/update_6.sql
@@ -1,4 +1,5 @@
 use crmdb;
 
-
-ALTER table activity_visiting_purpose_pl change activity_type parentId mediumint(9);
+-- ALTER table activity_visiting_purpose_pl change activity_type parentId mediumint(9);
+ALTER table activity_visiting_purpose_pl add parentId mediumint(9);
+update activity_visiting_purpose_pl set parentId = activity_type;
