dabffd6078971a4709a78280c4694f251f93a1a4
Feiyun <feiyun.zhou@gmail.com>
2013-11-10 11:54:29 +0800

    added package for the data import and export

---
+++ crm/src/main/java/com/rex/crm/dataport/DataImporter.java
@@ -0,0 +1,13 @@
+package com.rex.crm.dataport;
+
+public class DataImporter {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
