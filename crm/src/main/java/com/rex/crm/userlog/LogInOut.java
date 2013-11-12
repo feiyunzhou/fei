1f416e4898dcbc864b9f9f2e123619c02bf68b2d
Feiyun <feiyun.zhou@gmail.com>
2013-11-12 21:04:18 +0800

    added mission file

--- crm/src/main/java/com/rex/crm/userlog/LogInOut.java
+++ crm/src/main/java/com/rex/crm/userlog/LogInOut.java
@@ -1,29 +1,36 @@
 package com.rex.crm.userlog;
 
-public class LogInOut
-{
-  private String loginName;
-  public String getLoginName()
-  {
-    return loginName;
-  }
-  public void setLoginName(String loginName)
-  {
-    this.loginName = loginName;
-  }
-  public String getSessionId()
-  {
-    return sessionId;
-  }
-  public void setSessionId(String sessionId)
-  {
-    this.sessionId = sessionId;
-  }
-  private long logints;
-  public void setLogints(long logints)
-  {
-    this.logints = logints;
-  }
-  private String sessionId;
- 
+
+public class LogInOut extends LogObj {
+
+     private String loginName;
+     private String sessionId;
+     private long logints;
+     private long logoutts;
+     
+    public String getLoginName() {
+        return loginName;
+    }
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+    public String getSessionId() {
+        return sessionId;
+    }
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+    public long getLogints() {
+        return logints;
+    }
+    public void setLogints(long logints) {
+        this.logints = logints;
+    }
+    public long getLogoutts() {
+        return logoutts;
+    }
+    public void setLogoutts(long logoutts) {
+        this.logoutts = logoutts;
+    }
+     
 }
--- crm/src/main/java/com/rex/crm/userlog/LogObj.java
+++ crm/src/main/java/com/rex/crm/userlog/LogObj.java
@@ -1,17 +1,14 @@
 package com.rex.crm.userlog;
 
-public class LogObj
-{
-private String uuid;
+public class LogObj {
+    private String uuid;
 
-public String getUuid()
-{
-  return uuid;
-}
+    public String getUuid() {
+        return uuid;
+    }
 
-public void setUuid(String uuid)
-{
-  this.uuid = uuid;
-}
- 
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
 }
