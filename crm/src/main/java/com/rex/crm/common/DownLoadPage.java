0dfabc68622c770f81264d1eb36172534ba58cfc
ralf-yin <ralf.yin@gmail.com>
2013-11-15 11:18:47 +0800

    fix build error

--- crm/src/main/java/com/rex/crm/common/DownLoadPage.java
+++ crm/src/main/java/com/rex/crm/common/DownLoadPage.java
@@ -1,40 +1,13 @@
 package com.rex.crm.common;
 
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang3.tuple.Pair;
-import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.Radio;
-import org.apache.wicket.markup.html.form.RadioGroup;
-import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.link.Link;
-import org.apache.wicket.markup.html.list.ListItem;
-import org.apache.wicket.markup.html.list.ListView;
-import org.apache.wicket.markup.html.panel.FeedbackPanel;
-import org.apache.wicket.markup.html.panel.Fragment;
-import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
-import org.apache.wicket.model.PropertyModel;
-
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.rex.crm.AccountPage;
-import com.rex.crm.ActivitySelectPage;
 import com.rex.crm.DataPage;
 import com.rex.crm.SignIn2Session;
-import com.rex.crm.common.Entity;
-import com.rex.crm.common.Field;
-import com.rex.crm.common.FilterPanel;
-import com.rex.crm.common.PageableTablePanel;
-import com.rex.crm.common.CRUDPanel.Permissions;
-import com.rex.crm.db.DAOImpl;
-import com.rexen.crm.beans.UserRole;
 import com.rexen.crm.integration.DataExportDelegate;
-import com.rex.crm.util.CRMUtility;
 import com.rex.crm.util.Configuration;
 
 
