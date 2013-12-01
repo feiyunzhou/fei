8d0ee37a676b1ded99b18e887f642606c5b36491
DongJingChao <447958999@qq.com>
2013-12-01 14:34:40 +0800

    modify update_54 sql

--- crm/src/main/db/update_54.sql
+++ crm/src/main/db/update_54.sql
@@ -1,12 +1,11 @@
-DROP TABLE IF EXISTS `product`;
 CREATE TABLE `product` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(128) NOT NULL,
   `status` int(11) NOT NULL DEFAULT '1',
   `parentid` int(11) DEFAULT NULL,
   `specification` varchar(512) DEFAULT NULL,
-  `created_date` datetime DEFAULT CURRENT_TIMESTAMP,
-  `modified_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `created_date` datetime DEFAULT NULL,
+  `modified_date` datetime DEFAULT NULL,
   `created_by_user_id` int(11) DEFAULT NULL,
   `modfied_by_user_id` int(11) DEFAULT NULL,
   `level` int(11) DEFAULT NULL,
